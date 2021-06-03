eed0486afebb926a4613ac4e04db9cd53edffc6d
fuxiangyu <18115116016@163.com>
2021-06-03 18:28:54 +0800

    修改factory_address及Hash Code

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0xfb58dfF511c1426902F844942e6F70c6a0F4eD0c";
+export declare const INIT_CODE_HASH = "0xb9874a016ed6410b5154e02cb550df955b989884c5f34f3aeb438f3e237a3b6e";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
